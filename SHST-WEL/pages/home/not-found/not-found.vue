acff8a5890fb7f4ce8294ae925286075d346cb82
Czy <651525974@qq.com>
2020-09-02 11:34:13 +0800

    Refactor code - SHST-WEL

---
+++ SHST-WEL/pages/home/not-found/not-found.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<view class='status'>404</view>
+		<view class='status'>NOT</view>
+		<view class='status'>FOUND</view>
+		<navigator url="/pages/home/index/index" open-type="reLaunch" class='index' style='padding: 5px 0;'>返回首页</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.status {
+		text-align: center;
+		margin-top: 10px;
+		font-size: 80px;
+		color: #aaa;
+	}
+
+	.index {
+		margin-top: 20px;
+		font-size: 15px;
+		color: #aaa;
+		text-align: center;
+	}
+</style>
